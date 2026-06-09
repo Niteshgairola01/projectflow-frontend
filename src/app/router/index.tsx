0c539be3f8941b8372d5a5fcb923acf5a671dc6c
Niteshgairola01 <niteshgairolapapl@gmail.com>
2026-06-09 22:00:31 +0530

    Authentication: Public Route

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../../features/auth/pages/LoginPage";
+import { PublicRoute } from "./PublicRoute";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <LoginPage />,
+    element: (
+      <PublicRoute>
+        <LoginPage />
+      </PublicRoute>
+    ),
   },
 ]);
